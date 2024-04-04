23240bf0c8c4213ad5207cfe01ffd75db96a9e47
Joey Stipich <stipichj@gmail.com>
2024-04-04 16:01:05 -0400

    setup react app, add scss file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
+function mainDisplay() {
+
+  return (
+    <div>
+      <div id="timer-label">test</div>
+      <div id="time-left"></div>
+    </div>
+  )
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>productivity clock</header>
+      <mainDisplay/>
     </div>
   );
 }
