e1da836f1ed39f0103714f3fce6bb2157a80945f
Joey Stipich <stipichj@gmail.com>
2024-04-16 14:19:58 -0400

    implement responsive grid display

--- src/App.js
+++ src/App.js
@@ -110,7 +110,8 @@ function MainDisplay({setSessionLen, sessionLen, setBreakLen, breakLen, countdow
       }</div>
       <button className="main" id="start_stop" onClick={playPause}>Start/Stop</button>
       <button className="main" id="reset" onClick={reset}>reset</button>
-      <span id="tag">By Joey Stipich</span>
+      <span className="tag">By Joey Stipich</span>
+      <span className="tag credits">Background visuals by Chokchai Love King from "Amazing Nature"</span>
     </div>
   );
 }
